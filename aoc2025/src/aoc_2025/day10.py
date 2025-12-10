311a790a1b935deede3cccc591e4c58aa1561338
Matt Howard <mjhoward540@gmail.com>
2025-12-10 14:00:17 +0100

    Committing progress

--- aoc2025/src/aoc_2025/day10.py
+++ aoc2025/src/aoc_2025/day10.py
@@ -1,20 +1,22 @@
+from collections import deque
 from dataclasses import dataclass
-from functools import reduce
+from functools import lru_cache, reduce
 from itertools import combinations
-from typing import Any
+from typing import Any, Deque
 
 
 @dataclass
 class Entry:
-    # state_len: int
     final_state: int
+    state_len: int
     possible_press_masks: list[int]
+    joltages: tuple[int, ...]
 
 
-def build_diagram(s: str) -> int:
+def build_diagram(s: str):
     s = s[1:-1]
 
-    return set_bits([i for i, c in enumerate(s) if c == "#"])
+    return set_bits([i for i, c in enumerate(s) if c == "#"]), len(s)
 
 
 def build_toggles(s: list[str]) -> list[int]:
@@ -27,7 +29,19 @@ def build_toggles(s: list[str]) -> list[int]:
     return masks
 
 
+def build_joltages(s: str) -> tuple[int, ...]:
+    s = s[1:-1]
+    l: list[int] = []
+    for j in s.split(","):
+        l.append(int(j))
+
+    return tuple(l)
+
+
 def set_bits(bit_indices: list[int]) -> int:
+    """
+    Set bits to 1 at the given indices, then return the number representing those bits being set
+    """
     mask = 0
 
     for i in bit_indices:
@@ -36,17 +50,34 @@ def set_bits(bit_indices: list[int]) -> int:
     return mask
 
 
+# TODO is cache faster than loop?
+# @lru_cache
+def get_bits(n: int, length: int) -> tuple[int, ...]:
+    out = []
+    for _ in range(length):
+        out.append(n & 1)
+        n >>= 1
+    return tuple(out[::-1])
+
+
+def add_tuples(t1: tuple[int, ...], t2: tuple[int, ...]) -> tuple[int, ...]:
+    assert len(t1) == len(t2)
+
+    return tuple(n1 + n2 for n1, n2 in zip(t1, t2))
+
+
 def parse_input(inp: str):
     entries = []
     for line in inp.split("\n"):
         thing = line.split(" ")
-        diagram = build_diagram(thing[0])
+        diagram, state_len = build_diagram(thing[0])
 
-        joltages = thing[-1]
+        joltages = build_joltages(thing[-1])
+        print(joltages)
 
         toggles = build_toggles(thing[1:-1])
 
-        entries.append(Entry(diagram, toggles))
+        entries.append(Entry(diagram, state_len, toggles, joltages))
 
     return entries
 
@@ -76,6 +107,53 @@ def part1(entries: list[Entry]):
     return sum(l)
 
 
+def handle_entry2(entry: Entry) -> int:
+    zero = tuple([0 for _ in entry.joltages])
+    # mask to check, curr_joltages, curr_xor, curr number of presses
+    d: Deque[tuple[int, tuple[int, ...], int, int]] = deque(
+        [(mask, zero, 0, 0) for mask in entry.possible_press_masks]
+    )
+
+    # bfs
+    while d:
+        t = d.popleft()
+        print(t)
+        mask, curr_jolt, xor, num_presses = t
+
+        if curr_jolt == entry.joltages and xor == entry.final_state:
+            print("FOUND FOUND FOUND")
+            print("FOUND FOUND FOUND")
+            print("FOUND FOUND FOUND")
+            print("FOUND FOUND FOUND")
+            print("")
+            print("")
+
+            return num_presses
+        elif curr_jolt >= entry.joltages:
+            continue
+        else:
+            bits = get_bits(mask, entry.state_len)
+            new_jolt = add_tuples(bits, curr_jolt)
+            new_xor = mask ^ xor
+
+            deque.extend(
+                d,
+                (
+                    (next_mask, new_jolt, new_xor, num_presses + 1)
+                    for next_mask in entry.possible_press_masks
+                    if next_mask != mask
+                ),
+            )
+
+    return -1
+
+
+def part2(entries: list[Entry]):
+    l = [handle_entry2(entry) for entry in entries]
+    print(l)
+    return sum(l)
+
+
 if __name__ == "__main__":
     s = ""
     with open("input/2025/10.txt") as f:
@@ -83,5 +161,6 @@ if __name__ == "__main__":
 
     inp = parse_input(s)
     print("Part 1:", part1(inp))
+    print("Part 2:", part2(inp))
     # # 4653414735 too high
     # print("Part 2:", part2(inp))
