d1f2e185cf273a29cd634d37d54bcb007212bec0
Matt Howard <mjhoward540@gmail.com>
2025-12-11 10:03:42 +0100

    day 11 part 2

--- aoc2025/src/aoc_2025/day11.py
+++ aoc2025/src/aoc_2025/day11.py
@@ -1,4 +1,4 @@
-from collections import deque
+from functools import lru_cache
 
 PathMap = dict[str, list[str]]
 
@@ -15,35 +15,33 @@ def parse_input(s: str):
     return d
 
 
-def path_from(
-    d: PathMap, start: str, end: str, through: None | set[str]
-) -> tuple[list[str], int]:
-    path_count = 0
-    paths = []
-
-    to_visit = deque([(start, [])])
-
-    while to_visit:
-        node, curr_path = to_visit.popleft()
-
-        if node == end or d.get(node) is None:
-            if through and len(through) == len(through & set(curr_path)):
-                path_count += 1
-                paths.append([node] + curr_path)
-
-            continue
-
-        to_visit.extend((n, [node] + curr_path) for n in d[node])
-
-    return paths, path_count
-
-
 def part1(d: PathMap):
-    return path_from(d, "you", "out", set())
+    return paths_from("you", "out", d)
+
+
+def _paths_from(d: PathMap):
+    @lru_cache
+    def inner(node: str, end: str) -> int:
+        if node == end:
+            return 1
+
+        return sum(inner(n, end) for n in d.get(node, []))
+
+    return inner
+
+
+def paths_from(node: str, end: str, d: PathMap):
+    return _paths_from(d)(node, end)
 
 
 def part2(d: PathMap):
-    return path_from(d, "svr", "out", {"fft", "dac"})
+    svr_to_dac = paths_from("svr", "dac", d)
+    svr_to_fft = paths_from("svr", "fft", d)
+
+    if svr_to_dac < svr_to_fft:
+        return svr_to_dac * paths_from("dac", "fft", d) * paths_from("fft", "out", d)
+    else:
+        return svr_to_fft * paths_from("fft", "dac", d) * paths_from("dac", "out", d)
 
 
 if __name__ == "__main__":
@@ -52,7 +50,5 @@ if __name__ == "__main__":
         s = f.read()
 
     inp = parse_input(s)
-    # print("Part 1:", part1(inp))
+    print("Part 1:", part1(inp))
     print("Part 2:", part2(inp))
-    # # 4653414735 too high
-    # print("Part 2:", part2(inp))
