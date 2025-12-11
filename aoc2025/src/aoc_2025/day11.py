2e33d90a1fea3ab111d98cdc6a8e18347670cb55
Matt Howard <mjhoward540@gmail.com>
2025-12-11 09:27:54 +0100

    More tweaks

--- aoc2025/src/aoc_2025/day11.py
+++ aoc2025/src/aoc_2025/day11.py
@@ -1,8 +1,4 @@
 from collections import deque
-from dataclasses import dataclass
-from functools import lru_cache, reduce
-from itertools import combinations
-from typing import Any, Deque
 
 PathMap = dict[str, list[str]]
 
@@ -20,27 +16,34 @@ def parse_input(s: str):
 
 
 def path_from(
-    d: PathMap, start: str, end: str, through: None | list[str]
+    d: PathMap, start: str, end: str, through: None | set[str]
 ) -> tuple[list[str], int]:
     path_count = 0
     paths = []
 
-    to_visit = deque([start])
+    to_visit = deque([(start, [])])
 
     while to_visit:
-        node = to_visit.popleft()
+        node, curr_path = to_visit.popleft()
 
         if node == end or d.get(node) is None:
-            path_count += 1
+            if through and len(through) == len(through & set(curr_path)):
+                path_count += 1
+                paths.append([node] + curr_path)
+
             continue
 
-        to_visit.extend(d[node])
+        to_visit.extend((n, [node] + curr_path) for n in d[node])
 
     return paths, path_count
 
 
 def part1(d: PathMap):
-    return path_from(d, "you", "out", [])
+    return path_from(d, "you", "out", set())
+
+
+def part2(d: PathMap):
+    return path_from(d, "svr", "out", {"fft", "dac"})
 
 
 if __name__ == "__main__":
@@ -49,7 +52,7 @@ if __name__ == "__main__":
         s = f.read()
 
     inp = parse_input(s)
-    print("Part 1:", part1(inp))
-    # print("Part 2:", part2(inp))
+    # print("Part 1:", part1(inp))
+    print("Part 2:", part2(inp))
     # # 4653414735 too high
     # print("Part 2:", part2(inp))
